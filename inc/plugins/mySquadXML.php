d4534a4471cbb575103469a9cc04ac47e7072bd6
James McCartney <Jsm123@gmail.com>
2013-01-21 02:00:50 +0000

    Basic layout of the plugin PHP file

---
+++ inc/plugins/mySquadXML.php
@@ -0,0 +1,32 @@
+<?PHP
+
+function mySquadXML_info()
+{
+	return array(
+		"name"		=> "mySquadXML",
+		"description"		=> "Plugin for managing ArmA2 Squad.XML from inside of MyBB",
+		"website"		=> "",
+		"author"		=> "Jsm / BSO",
+		"authorsite"		=> "http://epic.ftp.sh",
+		"version"		=> "0.1",
+		"guid" 			=> "",
+		"compatibility"	=> "*"
+		);
+}
+function mySquadXML_install()
+{
+	
+}
+function mySquadXML_activate()
+{
+	
+}
+function mySquadXML_uninstall()
+{
+	
+}
+function mySquadXML_is_installed()
+{
+	
+}
+?>
